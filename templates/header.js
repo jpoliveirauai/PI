64aa65085a332c28949eb689e9c0d495dc242b0d
João Paulo de Oliveira <joaopaulodeoliveira123@gmail.com>
2019-09-21 18:04:36 -0300

    PI: Created project architecture

---
+++ templates/header.js
@@ -0,0 +1,19 @@
+document.write('<link rel="stylesheet" href="../css/templates.css"> <header class="cabecalho"> <nav> <ul class="menu"> <li class="menu-item"> <img class="logotipo" src="../img/tenor.png" alt="logotipo"> </li> <li class="menu-item">Casas mal assombradas</li> <li class="menu-item">Casas</li> <li class="menu-item">Apartamentos</li> <li class="menu-item">Sobre</li> <li class="menu-item">Campo de busca</li> <li class="menu-item">Login</li> </ul> </nav> </header>');
+// Para transformar o código html em uma linha, copie e cole na barra de navegação do Chrome
+{/* <link rel="stylesheet" href="../css/templates.css"> 
+<header class="cabecalho">
+<nav>
+    <ul class="menu">
+        <li class="menu-item">
+            <img class="logotipo" src="../img/tenor.png" alt="logotipo">
+        </li>
+        <li class="menu-item">Casas mal assombradas</li>
+        <li class="menu-item">Casas</li>
+        <li class="menu-item">Apartamentos</li>
+        <li class="menu-item">Sobre</li>
+        <li class="menu-item">Campo de busca</li>
+        <li class="menu-item">Login</li>
+    </ul>
+</nav>
+
+</header> */}
