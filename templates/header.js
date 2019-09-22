f8a4fd4605d2b38cd1c67fc4526d991190abed53
Gustavo <gnkenshin@gmail.com>
2019-09-22 18:29:58 -0300

    Mudança no header, e coloração

--- templates/header.js
+++ templates/header.js
@@ -1,19 +1,30 @@
-document.write('<link rel="stylesheet" href="../css/templates.css"> <header class="cabecalho"> <nav> <ul class="menu"> <li class="menu-item"> <img class="logotipo" src="../img/tenor.png" alt="logotipo"> </li> <li class="menu-item">Casas mal assombradas</li> <li class="menu-item">Casas</li> <li class="menu-item">Apartamentos</li> <li class="menu-item">Sobre</li> <li class="menu-item">Campo de busca</li> <li class="menu-item">Login</li> </ul> </nav> </header>');
+document.write('<header class="cabecalho"><nav><ul class="menu"><li class="menu-item"><figure><img class="logotipo" src="./img/tenor.png" alt="logotipo"></figure></li><li class="menu-item2"><form class ="form-inline"><select name = "condicao"><option value = "comprar" selected>Comprar</option><option value = "alugar">Alugar</option></select><select name = "tipo"><option value = "casamalassombrada">Casa mal assombrada</option><option value = "casa">Casa</option><option value = "apartamento" selected>Apartamento</option></select><input class="ajuste-item2" type = "text" name = "complemento"/><input class="ajuste-item2" type = "submit" value = "Buscar"/></form></li><li class="menu-item3"><button class="botao">Login</button></li></ul></nav></header>');
 // Para transformar o código html em uma linha, copie e cole na barra de navegação do Chrome
 {/* <link rel="stylesheet" href="../css/templates.css"> 
 <header class="cabecalho">
-<nav>
-    <ul class="menu">
-        <li class="menu-item">
-            <img class="logotipo" src="../img/tenor.png" alt="logotipo">
-        </li>
-        <li class="menu-item">Casas mal assombradas</li>
-        <li class="menu-item">Casas</li>
-        <li class="menu-item">Apartamentos</li>
-        <li class="menu-item">Sobre</li>
-        <li class="menu-item">Campo de busca</li>
-        <li class="menu-item">Login</li>
-    </ul>
-</nav>
-
+    <nav>
+        <ul class="menu">
+            <li class="menu-item">
+                <figure>
+                    <img class="logotipo" src="./img/tenor.png" alt="logotipo">
+                </figure>
+            </li>
+            <li class="menu-item2">
+                <form>
+                    <select name = "condicao">
+                    <option value = "comprar" selected>Comprar</option>
+                    <option value = "alugar">Alugar</option>
+                    </select>
+                    <select name = "tipo">
+                    <option value = "casamalassombrada">Casa mal assombrada</option>
+                    <option value = "casa">Casa</option>
+                    <option value = "apartamento" selected>Apartamento</option>
+                    </select>
+                    <input class = "ajuste-item2" type = "text" name = "complemento"/>
+                    <input class = "ajuste-item2" type = "submit" value = "Buscar"/>
+                </form>
+            </li>
+            <li class="menu-item3"><button class=".btn-danger">Login</button></li>
+        </ul>
+    </nav>
 </header> */}
