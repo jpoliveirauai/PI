538c784abd787dbf40d4c4da3543464826217e7d
Gustavo <gnkenshin@gmail.com>
2019-09-24 23:55:14 -0300

    Adicionado tela de login

--- templates/header.js
+++ templates/header.js
@@ -1,4 +1,4 @@
-document.write('<header class="cabecalho"><nav><ul class="menu"><li class="menu-item"><figure><img class="logotipo" src="./img/tenor.png" alt="logotipo"></figure></li><li class="menu-item2"><form class ="form-inline"><select name = "condicao"><option value = "comprar" selected>Comprar</option><option value = "alugar">Alugar</option></select><select name = "tipo"><option value = "casamalassombrada">Casa mal assombrada</option><option value = "casa">Casa</option><option value = "apartamento" selected>Apartamento</option></select><input class="ajuste-item2" type = "text" name = "complemento"/><input class="ajuste-item2" type = "submit" value = "Buscar"/></form></li><li class="menu-item3"><button class="botao">Login</button></li></ul></nav></header>');
+document.write('<header class="cabecalho"><nav><ul class="menu"><li class="menu-item"><figure><img class="logotipo" src="./img/tenor.png" alt="logotipo"></figure></li><li class="menu-item2"><form class ="form-inline"><select name = "condicao"><option value = "comprar" selected>Comprar</option><option value = "alugar">Alugar</option></select><select name = "tipo"><option value = "casamalassombrada">Casa mal assombrada</option><option value = "casa">Casa</option><option value = "apartamento" selected>Apartamento</option></select><input class="ajuste-item2" type = "text" name = "complemento"/><input class="ajuste-item2" type = "submit" value = "Buscar"/></form></li><li class="menu-item3"><button class="botao" data-toggle="modal" data-target="#logModal">Login</button></li></ul></nav></header>');
 // Para transformar o código html em uma linha, copie e cole na barra de navegação do Chrome
 {/* <link rel="stylesheet" href="../css/templates.css"> 
 <header class="cabecalho">
@@ -24,7 +24,7 @@ document.write('<header class="cabecalho"><nav><ul class="menu"><li class="menu-
                     <input class = "ajuste-item2" type = "submit" value = "Buscar"/>
                 </form>
             </li>
-            <li class="menu-item3"><button class=".btn-danger">Login</button></li>
+            <li class="menu-item3"><button class=".btn-danger" data-toggle="modal" data-target="#logModal>Login</button></li>
         </ul>
     </nav>
 </header> */}
