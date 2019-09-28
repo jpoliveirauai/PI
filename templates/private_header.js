7ee7bce1fcdb23135f41ba9eb7066d348fa90718
Matheus José <matheus.costa@zup.com.br>
2019-09-28 17:55:16 -0300

    Added  private header

---
+++ templates/private_header.js
@@ -0,0 +1,33 @@
+document.write('<link rel="stylesheet" href="../css/templates.css"><header class="row cabecalho" ><div class="col-md-4"><img class="logotipo" src="../img/tenor.png" alt="logotipo"></div><div class="col-md-2"><a href="../pages/lista-funcionarios.html" class="private-header-items">Funcionários</a></div><div class="col-md-2"><a href="../pages/lista-clientes.html" class="private-header-items">Clientes</a></div><div class="col-md-2"><a href="../pages/lista-imoveis.html" class="private-header-items">Imóveis</a></div><div class="col-md-1"><a href="#" class="private-header-items">Interesses</a></div><div class="col-md-1"><a href="#" class="private-header-items">Sair</a></div></header>  ');
+{/* <link rel="stylesheet" href="../css/templates.css">
+<header class="row cabecalho" >
+  <div class="col-md-4">
+    <img class="logotipo" src="../img/tenor.png" alt="logotipo">
+  </div>
+  <div class="col-md-2">
+      <a href="../pages/lista-funcionarios.html" class="private-header-items">
+        Funcionários
+      </a>
+  </div>
+  <div class="col-md-2">
+    <a href="../pages/lista-clientes.html" class="private-header-items">
+      Clientes
+    </a>
+  </div>
+  <div class="col-md-2">
+    <a href="../pages/lista-imoveis.html" class="private-header-items">
+      Imóveis
+    </a>
+  </div>
+  <div class="col-md-1">
+    <a href="#" class="private-header-items">
+      Interesses
+    </a>
+  </div>
+  <div class="col-md-1">
+    <a href="#" class="private-header-items">
+      Sair
+    </a>
+  </div>
+</header>   */}
+
