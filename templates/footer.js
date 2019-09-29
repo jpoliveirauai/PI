3992a29049667ded07f826798861aa105881da35
João Paulo de Oliveira <joaopaulodepliveira123@gmail.com>
2019-09-28 21:19:07 -0300

    Footer: Responsive footer

--- templates/footer.js
+++ templates/footer.js
@@ -1,40 +1,41 @@
-document.write('<link rel="stylesheet" href="../css/templates.css"> <footer class="rodape"> <section id="ts-footer-main"> <div class="container"> <div class="row"> <div class="col-md-6"> <img class="logotipo" src="../img/tenor.png" alt="logotipo"> <p class="mb-4"> A Zika Imobiliária é dominante no ramo imobiliário há 47 anos </p> <a href="#" class="btn btn-outline-dark mb-4 btn-sm">Conheça a Zika</a> <a href="#" class="btn btn-outline-dark mb-4 btn-sm">Contato</a> </div> <div class="col-md-4"> <h4>Contato</h4> <address class="ts-text-color-light">Av. João Naves de Ávila, 2121 - Uberlândia - MG <strong>Email: </strong> <a href="mailto:contato@zika.com.br" class="btn-link">contato@zika.com.br</a> <p><strong>Telefone:</strong> 34 3239 5000</p> <p><strong>Horário de atendimento: </strong> seg/sex - 8h às 18h sáb - 08h ás 12h</p> </address> </div> </div> </div> </section> <section id="ts-footer-secondary"> <div class="container"> <div class="ts-copyright"> © 2019, Zika empreendimentos imobiliários Ltda. Todos os direitos reservados. </div> </div> </section> <section id="ts-footer-secondary"> <div class="container justify-content-center"> </div> </section> </div> </div> </footer>');
-// Para transformar o código html em uma linha, copie e cole na barra de navegação do Chrome
-{/* <link rel="stylesheet" href="../css/templates.css"> 
-    <footer class="rodape">
-        <section id="ts-footer-main">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6">
-                        <img class="logotipo" src="../img/tenor.png" alt="logotipo">
-                        <p class="mb-4">
-                            A Zika Imobiliária é dominante no ramo imobiliário há 47 anos </p>
-                        <a href="#" class="btn btn-outline-dark mb-4 btn-sm">Conheça a Zika</a>
-                        <a href="#" class="btn btn-outline-dark mb-4 btn-sm">Contato</a>
-                    </div>
-                    <div class="col-md-4">
-                        <h4>Contato</h4>
-                        <address class="ts-text-color-light">Av. João Naves de Ávila, 2121 - Uberlândia - MG
-                            <strong>Email: </strong>
-                            <a href="mailto:contato@zika.com.br" class="btn-link">contato@zika.com.br</a>
-                            <p><strong>Telefone:</strong> 34 3239 5000</p>
-                            <p><strong>Horário de atendimento: </strong>
-                                seg/sex - 8h às 18h sáb - 08h ás 12h</p>
-                        </address>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <section id="ts-footer-secondary">
-            <div class="container">
-                <div class="ts-copyright">
-                    © 2019, Zika empreendimentos imobiliários Ltda. Todos os direitos reservados.
-                </div>
-            </div>
-        </section>
-        <section id="ts-footer-secondary">
-            <div class="container justify-content-center"> </div>
-        </section>
+document.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script><link rel="stylesheet" href="../css/templates.css"><footer class="rodape"><div class="row"><div class="col-md-4"><img class="logotipo" src="../img/tenor.png" alt="logotipo"><p class="mb-4">A Zika Imobiliária está no ramo imobiliário há 47 anos</p><p><strong>Horário de atendimento: </strong>seg/sex - 8h às 18h sáb - 08h ás 12h</p></div><div class="col-md-4"></div><div class="col-md-4"><h4>Contato</h4><address class="ts-text-color-light">Av. João Naves de Ávila, 2121 - Uberlândia - MG</address><strong>Email: </strong><a href="mailto:contato@zika.com.br" class="btn-link">contato@zika.com.br</a><p><strong>Telefone:</strong> 34 3239 5000</p></div></div><section><div class="row"><div class="col-md-12"><div class="ts-copyright">© 2019, Zika empreendimentos imobiliários Ltda. Todos os direitos reservados.</div></div></div></section><section><div class="container justify-content-center"> </div></section></footer>');
+
+{/* <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="../css/templates.css">
+<footer class="rodape">
+  <div class="row">
+    <div class="col-md-4">
+        <img class="logotipo" src="../img/tenor.png" alt="logotipo">
+        <p class="mb-4">
+          A Zika Imobiliária está no ramo imobiliário há 47 anos
+        </p>
+        <p><strong>Horário de atendimento: </strong>
+          seg/sex - 8h às 18h sáb - 08h ás 12h</p>
+          
+    </div>
+    <div class="col-md-4">
+    </div>
+    <div class="col-md-4">
+        <h4>Contato</h4>
+        <address class="ts-text-color-light">
+          Av. João Naves de Ávila, 2121 - Uberlândia - MG
+        </address>
+            <strong>Email: </strong>
+            <a href="mailto:contato@zika.com.br" class="btn-link">contato@zika.com.br</a>
+          <p><strong>Telefone:</strong> 34 3239 5000</p>
+    </div>
+  </div>
+  <section>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="ts-copyright">
+          © 2019, Zika empreendimentos imobiliários Ltda. Todos os direitos reservados.
         </div>
-        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="container justify-content-center"> </div>
+  </section>
 </footer> */}
